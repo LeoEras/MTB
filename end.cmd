1dbfd8e8aa834151514de28d3efc68cbc73fc788
LeoEras <leoded94@hotmail.com>
2017-01-17 23:14:13 -0500

    Update

--- end.cmd
+++ end.cmd
@@ -1,5 +1,5 @@
 @echo off
-git add .
-git commit . -m "Update"
-git checkout -b %var%
+git add . --quiet
+git commit . -m "Update" --quiet
+git checkout -b %var% --quiet
 git push https://LeoEras:hipoS666@github.com/LeoEras/MTB.git %var% --quiet
