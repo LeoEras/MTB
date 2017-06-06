b42eb10ddc85c53206c33846aa3e256fa6594251
Leras <leonardo.eras@cti.espol.edu.ec>
2017-06-06 16:15:48 -0500

    Update

--- end.cmd
+++ end.cmd
@@ -1,6 +1,6 @@
 @echo off
 git checkout -B 11 --quiet
 :: comment
-git add .
-git commit . -m "Update"
+git add . --quiet
+git commit . -m "Update" --quiet
 git push git+ssh://git@github.com/LeoEras/MTB.git 11 --quiet
