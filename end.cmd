88f4eeddf93ed1e43c9d61b051b6d23c4fd45e63
Leras <leonardo.eras@cti.espol.edu.ec>
2017-06-06 16:17:16 -0500

    Update

--- end.cmd
+++ end.cmd
@@ -1,6 +1,7 @@
 @echo off
+::Replace "11" with %var%
 git checkout -B 11 --quiet
-:: comment
 git add .
 git commit . -m "Update" --quiet
+::Replace "11" with %var% here too
 git push git+ssh://git@github.com/LeoEras/MTB.git 11 --quiet
